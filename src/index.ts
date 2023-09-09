4f7fc14f8d865ed3ed9c17794055d1b303de3815
Efen Stakes <efenstakes101@gmail.com>
2023-09-09 18:25:58 +0300

    add graphql resolvers and schema & start the graphql api

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,17 @@
 import express from "express";
+import { ApolloServer } from 'apollo-server-express'
 import path from "path";
 
 import cors from 'cors'
 import * as dotenv from 'dotenv'
 import morgan from 'morgan'
+
+import { buildSchema } from "type-graphql";
+import { ApolloServerPluginLandingPageProductionDefault, ApolloServerPluginLandingPageGraphQLPlayground, } from "apollo-server-core";
+import { connectToDb } from "./utils/database";
+import { resolvers } from "./resolvers";
+import { authChecker } from "./utils/authentication";
+
 
 const start = async ()=> {
 
@@ -14,6 +22,10 @@ const start = async ()=> {
     dotenv.config({ path: path.join(__dirname, '.env')})
     // console.log("path.join(__dirname, '.env') ", path.join(__dirname, '.env'));
     
+
+    // connect to db
+    connectToDb()
+
     // allow cross origin requests
     app.use(cors())
 
@@ -23,6 +35,30 @@ const start = async ()=> {
     app.use(express.json())
 
     console.log("env", process.env.DB_URL)
+
+
+    // create schema
+    const schema = await buildSchema({
+        resolvers,
+        authChecker,
+    })
+
+    // create server
+    const server = new ApolloServer({
+        schema,
+        plugins: [
+            // process.env.NODE_ENV === 'production'
+            //     ? ApolloServerPluginLandingPageProductionDefault()
+            //     : ApolloServerPluginLandingPageGraphQLPlayground()
+        ],
+        context : ({ req, res, }) => {
+
+            return { req, res, user: req['user'], }
+        },
+    });
+
+    await server.start()
+    server.applyMiddleware({ app, path: '/api' })
 
 
     app.get("/", (_req, res)=> {
