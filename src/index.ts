329856fef95ccc5f7572b6cc957bf98d6c00a8c2
Efen Stakes <efenstakes101@gmail.com>
2023-09-09 18:24:57 +0300

    add basic express server

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,44 @@
+import express from "express";
+import path from "path";
 
+import cors from 'cors'
+import * as dotenv from 'dotenv'
+import morgan from 'morgan'
+
+const start = async ()=> {
+
+    // create express server
+    const app = express()
+
+    // get environment variables
+    dotenv.config({ path: path.join(__dirname, '.env')})
+    // console.log("path.join(__dirname, '.env') ", path.join(__dirname, '.env'));
+    
+    // allow cross origin requests
+    app.use(cors())
+
+    // show dev logs
+    app.use(morgan('dev'))
+
+    app.use(express.json())
+
+    console.log("env", process.env.DB_URL)
+
+
+    app.get("/", (_req, res)=> {
+
+        res.json({
+            "api": "Phishing Guard",
+            running: true
+        })
+    })
+
+
+    const PORT = process.env.PORT
+    app.listen(PORT, ()=> {
+
+        console.log(`api running on ${PORT}`);
+    })
+}
+
+start()
